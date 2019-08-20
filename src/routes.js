cfa6bf3dab69dd6224e33a143225220109b8ecaf
Felipe Homrich Melchior <fehmel@gmail.com>
2019-08-20 15:35:48 -0300

    feat: add connection db / loader models

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'OK' });
-});
+routes.post('/users');
 
 export default routes;
